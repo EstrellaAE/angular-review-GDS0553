7191e027c98963ba597dcd645a5fc6bcbd1c0191
EstrellaAE <eaguayoexiga@gmail.com>
2024-01-19 11:30:42 -0600

    estrella

--- src/app/app.module.server.ts
+++ src/app/app.module.server.ts
@@ -3,11 +3,16 @@ import { ServerModule } from '@angular/platform-server';
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
+import { ButtonModule } from 'primeng/button';
+
+
 
 @NgModule({
   imports: [
     AppModule,
     ServerModule,
+    ButtonModule,
+    
   ],
   bootstrap: [AppComponent],
 })
